df6fabedf8c634d680a53415d67aef3889d29a95
bottagoz <118192999+bottagoz@users.noreply.github.com>
2023-01-11 17:33:42 +0600

    Add files via upload

---
+++ lecture 12/index.js
@@ -0,0 +1,14 @@
+// HW 
+// task1 (show the average)
+
+// let a = +prompt('enter a number, please');
+// let b = +prompt('enter one more number, please');
+// alert((a+b)/2);
+
+// HW 
+// task2 (show the remainder)
+
+let a = +prompt('enter a number, please');
+let b = +prompt('enter one more number, please');
+alert(a%b);
+
