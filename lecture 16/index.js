20d777f3131e12309afe14119d2749476eeeefaf
bottagoz <118192999+bottagoz@users.noreply.github.com>
2023-01-18 16:18:45 +0600

    Add files via upload

---
+++ lecture 16/index.js
@@ -0,0 +1,80 @@
+// HW
+// task1 
+
+// let sapsanStudent = {
+//     name: 'Saule',
+//     surname: 'Nazarbayeva'
+// }
+// console.log(sapsanStudent);
+
+// sapsanStudent.name = 'Akmaral',
+// console.log(sapsanStudent);
+
+// delete sapsanStudent.name;
+// console.log(sapsanStudent);
+
+// HW
+// task2 не смогла сделать
+
+// HW
+// task3
+
+let greatparents = {
+    greatparent1: {
+        firstname: 'Maulen',
+        member: 'grandpa',
+        gender: 'male',
+        birthdate: 1900
+    },
+    greatparent2: {
+        firstname: 'Zaureeeeeee',
+        member: 'grandma',
+        gender: 'female',
+        birthdate: 19000000
+    },
+    greatparent3: {
+        firstname: 'Toleubai',
+        member: 'grandpa',
+        gender: 'male',
+        birthdate: 1900000000
+    },
+    greatparent4: {
+        firstname: 'Khanifa',
+        member: 'grandma',
+        gender: 'female',
+        birthdate: 1900000000
+    },
+}
+console.log(greatparents);
+
+let parents = {
+    parent1: {
+        firstname: 'Alpysbay',
+        member: 'pa',
+        gender: 'male',
+        birthdate: 1960
+    },
+    parent2: {
+        firstname: 'Roza',
+        member: 'ma',
+        gender: 'female',
+        birthdate: 1966
+    }
+}
+console.log(parents);
+
+let offspring = {
+    offspring1: {
+        firstname: 'Botagoz',
+        member: 'daughter',
+        gender: 'female',
+        birthdate: 1992
+    },
+    offspring2: {
+        firstname: 'Almas',
+        member: 'son',
+        gender: 'male',
+        birthdate: 1996
+    }
+}
+console.log(offspring);
